bbd7395e42f22abbef844419e321bef17837d401
shivam <shivam.dhuria07@gmail.com>
2021-06-21 23:30:14 +0530

    fixed formatting

--- app/src/main/java/com/elixer/paws/interactors/GetDogs.kt
+++ app/src/main/java/com/elixer/paws/interactors/GetDogs.kt
@@ -1,8 +1,8 @@
 package com.elixer.paws.interactors
 
 import com.elixer.paws.DogResponse
-import com.elixer.paws.ErrorMapper
-import com.elixer.paws.ErrorMapper.Companion.INTERNET_CONNECTION_ERROR
+import com.elixer.paws.ErrorMessages
+import com.elixer.paws.ErrorMessages.Companion.INTERNET_CONNECTION_ERROR
 import com.elixer.paws.RetrofitService
 import com.elixer.paws.ui.ResultWrapper
 import com.google.gson.Gson
@@ -13,43 +13,64 @@ import java.io.IOException
 
 class GetDogs(private val retrofitService: RetrofitService) {
 
-    fun execute(breed: String): Flow<ResultWrapper<String>> = flow {
-        try {
-            val dogs = getDogsFromNetwork(breed)
-            //Get resource id for a 200 success response
-            emit(ResultWrapper.Success(dogs.message,statusResourceId = ErrorMapper.mapOf(200)))
-        } catch (throwable: Throwable) {
-            when (throwable) {
-                is IOException -> emit(
-                    //Get resource id for no internet connection
-                    ResultWrapper.GenericError(statusResourceId = ErrorMapper.mapOf(INTERNET_CONNECTION_ERROR)))
-                is HttpException -> {
-                    val errorResponse = convertErrorBody(throwable)
-                    emit(errorResponse)
-                }
-                else -> {
-                    returnGenericStatusError()
-                }
-            }
+  fun execute(breed: String): Flow<ResultWrapper<String>> =
+    flow {
+      try {
+        val dogs = getDogsFromNetwork(breed)
+        //Get resource id for a 200 success response
+        emit(
+          ResultWrapper.Success(
+            dogs.message,
+            statusResourceId = ErrorMessages.resourceIdFor(
+              200
+            )
+          )
+        )
+      } catch (throwable: Throwable) {
+        when (throwable) {
+          is IOException -> emit(
+            //Get resource id for no internet connection
+            ResultWrapper.GenericError(
+              statusResourceId = ErrorMessages.resourceIdFor(
+                INTERNET_CONNECTION_ERROR
+              )
+            )
+          )
+          is HttpException -> {
+            val errorResponse = convertErrorBody(throwable)
+            emit(errorResponse)
+          }
+          else -> {
+            returnGenericStatusError()
+          }
         }
+      }
     }
 
-    private fun convertErrorBody(throwable: HttpException): ResultWrapper.GenericError {
-        return try {
-            throwable.response()?.errorBody()?.let {
-                val errorResponse = Gson().fromJson(it.charStream(), DogResponse::class.java)
-                //Get resource id for the "code" value in error Response from the server
-                return ResultWrapper.GenericError(ErrorMapper.mapOf(errorResponse.code))
-            } ?: returnGenericStatusError()
-        } catch (exception: Exception) {
-            return returnGenericStatusError()
-        }
+  private fun convertErrorBody(throwable: HttpException): ResultWrapper.GenericError {
+    return try {
+      throwable.response()?.errorBody()?.let {
+        val errorResponse = Gson().fromJson(
+          it.charStream(),
+          DogResponse::class.java
+        )
+        //Get resource id for the "code" value in error Response from the server
+        return ResultWrapper.GenericError(
+          ErrorMessages.resourceIdFor(
+            errorResponse.code
+          )
+        )
+      } ?: returnGenericStatusError()
+    } catch (exception: Exception) {
+      return returnGenericStatusError()
     }
+  }
 
-    private fun returnGenericStatusError() = ResultWrapper.GenericError(ErrorMapper.mapOf(0))
+  private fun returnGenericStatusError() =
+    ResultWrapper.GenericError(ErrorMessages.resourceIdFor(0))
 
-    private suspend fun getDogsFromNetwork(breed: String): DogResponse {
-        val dogArray = retrofitService.getDog(breed)
-        return dogArray
-    }
+  private suspend fun getDogsFromNetwork(breed: String): DogResponse {
+    val dogArray = retrofitService.getDog(breed)
+    return dogArray
+  }
 }
--- app/src/main/java/com/elixer/paws/ui/MainActivity.kt
+++ app/src/main/java/com/elixer/paws/ui/MainActivity.kt
@@ -13,42 +13,54 @@ import kotlinx.android.synthetic.main.activity_main.*
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainActivityViewModel by viewModels()
+  private val viewModel: MainActivityViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button.setOnClickListener() {
-            viewModel.loadImage(edittext_breed.text.toString())
-            try {
-                val imm: InputMethodManager =
-                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
-            } catch (e: Exception) {
-            }
-        }
-        observerLiveData()
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    button.setOnClickListener() {
+      viewModel.loadImage(edittext_breed.text.toString())
+      try {
+        val imm: InputMethodManager =
+          getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(
+          currentFocus!!.windowToken,
+          0
+        )
+      } catch (e: Exception) {
+      }
     }
+    observerLiveData()
+  }
 
-    private fun observerLiveData() {
+  private fun observerLiveData() {
 
-        viewModel.imageUrl.observe(this, { image ->
-            if (image.isNullOrBlank()) {
-                MyImageLoader.loadImage(this, R.drawable.placeholder, imageView_dog)
-            } else {
-                MyImageLoader.loadImage(this, image, imageView_dog)
-            }
-        })
+    viewModel.imageUrl.observe(this, { image ->
+      if (image.isNullOrBlank()) {
+        MyImageLoader.loadImage(
+          this,
+          R.drawable.placeholder,
+          imageView_dog
+        )
+      } else {
+        MyImageLoader.loadImage(this, image, imageView_dog)
+      }
+    })
 
 //        viewModel.status.observe(this, { status ->
 //            textview_status.text = status
 //        })
 
-        viewModel.statusResourceId.observe(this, { statusResourceId ->
-            textview_status.text = statusResourceId?.let { getString(it) }
-        })
-        viewModel.loading.observe(this, { isLoading ->
-            progressBar.visibility = if (isLoading) View.VISIBLE else View.INVISIBLE
-        })
-    }
+    viewModel.statusResourceId.observe(
+      this,
+      { statusResourceId ->
+        textview_status.text = statusResourceId?.let {
+          getString(it)
+        }
+      })
+    viewModel.loading.observe(this, { isLoading ->
+      progressBar.visibility =
+        if (isLoading) View.VISIBLE else View.INVISIBLE
+    })
+  }
 }
